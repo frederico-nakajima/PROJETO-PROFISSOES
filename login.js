cbe341304612e8a5cff395de0d3c57ca4ef8b144
frederico nakajima <fredericonakajimanakajima@gmail.com>
2024-03-21 09:10:00 -0300

    ajustes

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ for(let i = 0; i < 11; i++){
 
     const size = Math.floor(random(10,120)) 
 
-    const position = random(1,80) 
+    const position = random(1,75) 
     const delay = random(5, 0.1)
     const duration = random(24,12)
 
