b9f71d876773a63b2561174d98dc6719075e71cb
frederico nakajima <fredericonakajimanakajima@gmail.com>
2024-03-21 04:33:20 -0300

    ajustes animação para tela mobile

--- login.js
+++ login.js
@@ -9,18 +9,19 @@ for(let i = 0; i < 11; i++){
 
     const size = Math.floor(random(10,120)) 
 
-    const position = random(1,99) 
+    const position = random(1,85) 
     const delay = random(5, 0.1)
     const duration = random(24,12)
 
     li.style.width = `${size}px`
     li.style.height = `${size}px`
-    li.style.bottom = `-${size}px`
+    li.style.bottom = `${size}px`
 
     li.style.left = `${position}%`
 
     li.style.animationDelay = `${delay}s`
     li.style.animationDuration = `${duration}s`
+    li.style.opacity = '0';
 
 
 
