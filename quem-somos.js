08a0bbb795cc1e9aac44ef05e3bdf56645fd59f9
frederico nakajima <fredericonakajimanakajima@gmail.com>
2024-03-27 14:34:23 -0300

    ajustes

--- quem-somos.js
+++ quem-somos.js
@@ -1,5 +1,9 @@
-const faq = document.getElementById('faq')
+
 const resposta = document.getElementById('resposta')
+const resposta1 = document.getElementById('resposta1')
+const resposta2 = document.getElementById('resposta2')
+const resposta3 = document.getElementById('resposta3')
+const resposta4 = document.getElementById('resposta4')
 const expand_less = document.getElementById('expand_less')
 const expand_more = document.getElementById('expand_more')
 
@@ -10,6 +14,62 @@ function feedBack(){
     expand_more.style.display = 'block'
 }else{
       resposta.style.display = 'none'
+    expand_less.style.display = 'block'
+    expand_more.style.display = 'none'
+
+}
+
+}
+
+function feedBack1(){
+    if(resposta1.style.display === 'none'){
+    resposta1.style.display = 'block'
+    expand_less.style.display = 'none'
+    expand_more.style.display = 'block'
+}else{
+      resposta1.style.display = 'none'
+    expand_less.style.display = 'block'
+    expand_more.style.display = 'none'
+
+}
+
+}
+
+function feedBack2(){
+    if(resposta2.style.display === 'none'){
+    resposta2.style.display = 'block'
+    expand_less.style.display = 'none'
+    expand_more.style.display = 'block'
+}else{
+      resposta2.style.display = 'none'
+    expand_less.style.display = 'block'
+    expand_more.style.display = 'none'
+
+}
+
+}
+
+function feedBack3(){
+    if(resposta3.style.display === 'none'){
+    resposta3.style.display = 'block'
+    expand_less.style.display = 'none'
+    expand_more.style.display = 'block'
+}else{
+      resposta3.style.display = 'none'
+    expand_less.style.display = 'block'
+    expand_more.style.display = 'none'
+
+}
+
+}
+
+function feedBack4(){
+    if(resposta4.style.display === 'none'){
+    resposta4.style.display = 'block'
+    expand_less.style.display = 'none'
+    expand_more.style.display = 'block'
+}else{
+      resposta4.style.display = 'none'
     expand_less.style.display = 'block'
     expand_more.style.display = 'none'
 
